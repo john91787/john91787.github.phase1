6adbe3f64f2bba52735775c9f0e4a9ae3f45480c
jonathan <jhector39@gmail.com>
2021-04-25 21:19:26 -0400

    all done now

--- index.js
+++ index.js
@@ -5,36 +5,39 @@ const getQuote = () => {
 }
 
 
-const makeQuoteDiv = (quote) => {    
+const awsemQuoteDiv = (quote) => {    
     const div = document.createElement('div')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
+    const h2 = document.createElement("h2")
     console.log(quote);
     h3.innerText = quote[0].author
     p.innerText = quote[0].quote
+    h2.innertext = quote[0].series
     
     div.appendChild(h3)
     div.appendChild(p)
+    div.appendChild(h2)
     
     return div
 }
 
 const appendQuote = (quoteDiv) => {
-    const quoteContainer = document.getElementById('quote-container')
+    const quoteContainer = document.getElementById('quote-box')
     quoteContainer.innerHTML = ""
     quoteContainer.appendChild(quoteDiv)
 }
 
 
 getQuote().then(quote => {
-    const quoteDiv = makeQuoteDiv(quote) 
+    const quoteDiv = awsemQuoteDiv(quote) 
     appendQuote(quoteDiv)
 })
 
 const button = document.getElementById('new-quote-btn')
 button.addEventListener('click', () => {
     getQuote().then(quote => {
-        const quoteDiv = makeQuoteDiv(quote) 
+        const quoteDiv = awsemQuoteDiv(quote) 
         appendQuote(quoteDiv)
     })
 })
